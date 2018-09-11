0220bb97b3a637c1f2ff21e2ac6085cffd191e9a
Sergey Rozhkov <s.rozhkov@andersenlab.com>
2018-09-11 16:46:24 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const stack = [];
+
+    for (let chr of str) {
+        if (stack.length === 0) {
+            stack.push(chr);
+
+            continue;
+        }
+
+        for (let brackets of bracketsConfig) {
+            if (brackets.includes(chr)) {
+                if (brackets[0] === brackets[1]) {
+                    const last = stack.pop();
+
+                    if (chr !== last) {
+                        stack.push(last);
+                        stack.push(chr);
+                    }
+                } else if (chr === brackets[0]) {
+                    // open bracket
+                    stack.push(chr);
+                } else if (chr === brackets[1]) {
+                    // close bracket
+                    const last = stack.pop();
+
+                    if (last !== brackets[0]) {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
